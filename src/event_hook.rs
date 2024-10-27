37f17ded460ceda2dc38f1b389b1b6197b0f57f0
Luke You <lukeyou78@gmail.com>
2024-10-27 03:20:17 -0500

    fixed cloaked event?

--- src/event_hook.rs
+++ src/event_hook.rs
@@ -74,9 +74,7 @@ pub extern "system" fn handle_win_event_main(
             }
         },
         EVENT_OBJECT_CLOAKED => {
-            if unsafe { !IsWindowVisible(hwnd).as_bool() } {
-                destroy_border_thread(hwnd);
-            }
+            destroy_border_thread(hwnd);
         },
         _ => {}
     }
