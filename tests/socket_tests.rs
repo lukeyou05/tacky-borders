1a66aa3279d39f374667a943d0577e6839e2ccfa
lukeyou05 <44282033+lukeyou05@users.noreply.github.com>
2025-08-27 18:53:06 -0500

    refactor: sockets refactor and sync support (#48)
    * feat: support synchronous socket operations
    
    * fix: reset variables between operations as per MSDN recommendation
    
    * refactor: separate overlapped and non-overlapped i/o methods
    
    * refactor: separate overlapped-specific fields into their own struct
    
    * test: ensure the number of bytes transferred during a socket read/write is correct
    
    * refactor: replace manual winsock startup with automatic startup inside socket methods
    
    * refactor: implement io::Write and io::Read for UnixStream, and use io::Write::write_all
    
    * refactor: replace anyhow::Result with io::Result for CompletionPort
    
    * refactor: add context to stream/listener/iocp errors
    
    * refactor: replace most anyhow::Result with io::Result for socket-related methods
    
    * refactor: mark UnixDomainSocket's overlapped methods as unsafe

--- tests/socket_tests.rs
+++ tests/socket_tests.rs
@@ -1,26 +1,19 @@
+use std::io::{Read, Write};
 use std::{fs, thread};
 
-use anyhow::{Context, anyhow};
+use anyhow::Context;
 use tacky_borders::config::Config;
 use tacky_borders::iocp::{CompletionPort, UnixListener, UnixStream};
-use windows::Win32::Networking::WinSock::{WSACleanup, WSADATA, WSAStartup};
+use tacky_borders::utils::remove_file_if_exists;
 use windows::Win32::System::IO::OVERLAPPED_ENTRY;
 
 #[test]
-fn test_socket_write_read() -> anyhow::Result<()> {
-    // Start up the WinSock Service
-    let iresult = unsafe { WSAStartup(0x202, &mut WSADATA::default()) };
-    if iresult != 0 {
-        return Err(anyhow!("WSAStartup failure: {iresult}"));
-    }
-
-    let socket_path = Config::get_dir()?.join("test.sock");
+fn test_socket_write_read_overlapped() -> anyhow::Result<()> {
+    let socket_path = Config::get_dir()?.join("test_overlapped.sock");
     let socket_path_clone = socket_path.clone();
 
     // If the socket file already exists, we cannot bind to it, so we must delete it first
-    if fs::exists(&socket_path).context("could not check if test socket exists")? {
-        fs::remove_file(&socket_path)?;
-    }
+    remove_file_if_exists(&socket_path).context("could not remove socket if exists")?;
 
     let port = CompletionPort::new(2)?;
 
@@ -29,19 +22,25 @@ fn test_socket_write_read() -> anyhow::Result<()> {
     port.associate_handle(listener.socket.to_handle(), listener.token())?;
 
     // Queue up an accept operation (asynchronous)
-    let mut read_stream = listener.accept()?;
+    let mut read_stream = unsafe { listener.accept_overlapped() }?;
     port.associate_handle(read_stream.socket.to_handle(), read_stream.token())?;
 
     let mut text = "Hello World!".to_string();
     let mut text_clone = text.clone();
 
-    thread::spawn(move || -> anyhow::Result<()> {
+    let join_handle = thread::spawn(move || -> anyhow::Result<u32> {
+        let port_2 = CompletionPort::new(1)?;
+
         let mut write_stream = UnixStream::connect(&socket_path_clone)?;
+        port_2.associate_handle(write_stream.socket.to_handle(), write_stream.token())?;
+
         let input_buffer = unsafe { text_clone.as_bytes_mut() };
+        unsafe { write_stream.write_overlapped(input_buffer) }?;
 
-        write_stream.write(input_buffer)?;
+        let mut entry = OVERLAPPED_ENTRY::default();
+        port_2.poll_single(None, &mut entry)?;
 
-        Ok(())
+        Ok(entry.dwNumberOfBytesTransferred)
     });
 
     // Wait for the listener to asynchronously accept the connection
@@ -50,14 +49,64 @@ fn test_socket_write_read() -> anyhow::Result<()> {
 
     // Then, queue up a read operation and wait for that to asynchronously finish
     let output_buffer = vec![0u8; text.len()];
-    read_stream.read(output_buffer)?;
+    unsafe { read_stream.read_overlapped(output_buffer) }?;
     port.poll_single(None, &mut entry)?;
 
     fs::remove_file(&socket_path)?;
-    unsafe { WSACleanup() };
+
+    assert!(join_handle.is_finished());
+
+    let bytes_written = join_handle
+        .join()
+        .expect("could not join write_stream's thread")?;
+    let bytes_read = entry.dwNumberOfBytesTransferred;
+    let output_buffer = read_stream
+        .take_overlapped_buffer()
+        .context("read_stream's buffer is None")?;
+    let correct_output = unsafe { text.as_bytes_mut() };
+
+    assert!(bytes_written as usize == correct_output.len());
+    assert!(bytes_read as usize == correct_output.len());
+    assert!(output_buffer == correct_output);
+
+    Ok(())
+}
+
+#[test]
+fn test_socket_write_read() -> anyhow::Result<()> {
+    let socket_path = Config::get_dir()?.join("test.sock");
+    let socket_path_clone = socket_path.clone();
+
+    // If the socket file already exists, we cannot bind to it, so we must delete it first
+    remove_file_if_exists(&socket_path).context("coulud not remove socket if exists")?;
+
+    let listener = UnixListener::bind(&socket_path)?;
+
+    let mut text = "Hello World!".to_string();
+    let mut text_clone = text.clone();
+
+    let join_handle = thread::spawn(move || -> anyhow::Result<()> {
+        let mut write_stream = UnixStream::connect(&socket_path_clone)?;
+
+        let input_buffer = unsafe { text_clone.as_bytes_mut() };
+        write_stream.write_all(input_buffer)?;
+
+        Ok(())
+    });
+
+    let mut read_stream = listener.accept()?;
+
+    let mut output_buffer = vec![0u8; text.len()];
+    let bytes_read = read_stream.read(&mut output_buffer)?;
+
+    fs::remove_file(&socket_path)?;
+
+    assert!(join_handle.is_finished());
 
     let correct_output = unsafe { text.as_bytes_mut() };
-    assert!(read_stream.buffer == correct_output);
+
+    assert!(bytes_read == correct_output.len());
+    assert!(output_buffer == correct_output);
 
     Ok(())
 }
